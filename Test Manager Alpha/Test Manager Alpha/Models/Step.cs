3bfe25597ff63cf1a4f41779ec4a40b4dc1e92d1
MGushchin <uevfrc@gmail.com>
2023-11-14 23:16:34 +0700

    -Test Manager Project
    Removed Test Manager Project. Now this repository is only for old projects

--- Test Manager Alpha/Test Manager Alpha/Models/Step.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Test_Manager_Alpha.Models
-{
-    public class Step
-    {
-        public int Id { get; set; }
-        public string? Action { get; set; }
-        public string? ExpectedResult { get; set; }
-    }
-}
